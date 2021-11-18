e2b3e214dcdc9a17f8c3cae42c24453bba94a680
Sergio Torres <sdtorresl@unal.edu.co>
2021-11-18 00:14:41 -0500

    Fix misspell

--- client/js/rooms.js
+++ client/js/rooms.js
@@ -2,4 +2,4 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const roomSid = urlParams.get("id")
 
-console.log(`Joining roon with SID ${roomSid}`)
+console.log(`Joining room with SID ${roomSid}`)
